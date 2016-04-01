0ce4cb9e5027ace7f9a5e4719cbc9a70c68213a5
Philip <greecephilip@yahoo.co.uk>
2016-04-01 22:30:22 +0300

    new line

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -25,6 +25,7 @@ namespace App1
             Button button = FindViewById<Button>(Resource.Id.MyButton);
             // lol change
             // new line
+            // another new line
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
